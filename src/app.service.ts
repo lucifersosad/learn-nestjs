be99542d211b670acec18100212868d8853476bc
Lucifer <luciferxhnh@gmail.com>
2025-06-29 16:21:01 +0700

    fix: add deploy flag build

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! - Test again';
+    return 'Hello World! - Test again #1';
   }
 }
